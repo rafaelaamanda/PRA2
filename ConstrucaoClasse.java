198a975f02f85ee84df482951d98b2c3e797ef45
rafaelaamanda <65137413+rafaelaamanda@users.noreply.github.com>
2020-05-11 12:38:51 -0300

    Create ConstrucaoClasse.java

---
+++ ConstrucaoClasse.java
@@ -0,0 +1,70 @@
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+@Entity(tableName = "ocurrences")
+
+public class Ocorrence {
+@PrimaryKey(autoGenerate = true)
+    @NonNull
+    @ColumnInfo(name = "ocurrenceId")
+    private int oId;
+    @Nullable
+    @ColumnInfo(name = "ocurrenceDescription")
+    private String oDescription;
+    
+    public int getId() {
+          return oId; 
+    }
+
+    public void setId(@NonNull int id) {
+        oId = id;
+    }
+
+    public String getDescription() {
+        return oDescription;
+    }
+
+    public void setDescription(@Nullable String description) {
+        oDescription = description;
+    }
+
+    public String getForwarding() {
+        return oForwarding;
+    }
+
+    public void setForwarding(String forwarding) {
+        oForwarding = forwarding;
+    }
+
+    public int getGrade() {
+        return oGrade;
+    }
+
+    public void setGrade(int grade) {
+        oGrade = grade;
+    }
+
+    public String getCreationDate() {
+        return oCreationDate;
+    }
+
+    public void setCreationDate(String creationDate) {
+        oCreationDate = creationDate;
+    }
+    private String oForwarding
+    private String oCreationDate;
+    public int getGrade() {
+    return oGrade;
+    }
+    public void setGrade(int grade) {
+    oGrade = grade;
+    }
+    private int oGrade;
+    public Ocurrence(String description, String forwarding, int grade, String creationDate) {
+        oDescription = description;
+        oForwarding = forwarding;
+        oGrade = grade;
+        oCreationDate = creationDate;
+    }
